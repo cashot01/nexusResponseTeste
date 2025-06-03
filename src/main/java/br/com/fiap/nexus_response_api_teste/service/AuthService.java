767c7bdac1d96b6a486ec7892cc328cf6903d7c2
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-06-03 18:49:25 -0300

    auth service

--- src/main/java/br/com/fiap/nexus_response_api_teste/service/AuthService.java
+++ src/main/java/br/com/fiap/nexus_response_api_teste/service/AuthService.java
@@ -1,18 +1,16 @@
 package br.com.fiap.nexus_response_api_teste.service;
 
-import br.com.fiap.nexus_response_api_teste.model.Credenciais;
-import br.com.fiap.nexus_response_api_teste.model.Token;
-
-import br.com.fiap.nexus_response_api_teste.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.BadCredentialsException;
-
-
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import br.com.fiap.nexus_response_api_teste.model.Credenciais;
+import br.com.fiap.nexus_response_api_teste.model.Token;
+import br.com.fiap.nexus_response_api_teste.repository.UsuarioRepository;
 
 @Service
 public class AuthService implements UserDetailsService {
@@ -30,21 +28,21 @@ public class AuthService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         var user = repository.findByEmail(username).orElseThrow(
                 () -> new UsernameNotFoundException(username, null));
-        System.out.println("Usuario encotrado:" + user);
+        System.out.println("User found:" + user);
         return user;
     }
 
-    public Token login(Credenciais credenciais){
-        var usuario = repository.findByEmail(credenciais.email())
-                .orElseThrow( () -> new UsernameNotFoundException("Usuário não encontrado", null));
+    public Token login(Credenciais credenciais) {
+        var user = repository.findByEmail(credenciais.email())
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado", null));
 
-        if(!passwordEncoder.matches(credenciais.senha(), usuario.getSenha()) ){
+        if (!passwordEncoder.matches(credenciais.senha(), user.getSenha())) {
             throw new BadCredentialsException("Senha incorreta");
         }
 
-        return tokenService.createToken(usuario);
+        return tokenService.createToken(user);
     }
-
-
-
 }
+
+
+
