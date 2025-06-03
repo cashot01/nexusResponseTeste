382ca5e42d8cea2a5ee615d09d7b78ff5adc2932
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-06-03 17:41:28 -0300

    projeto nexus Response teste

---
+++ src/main/java/br/com/fiap/nexus_response_api_teste/NexusResponseApiTesteApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.nexus_response_api_teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusResponseApiTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusResponseApiTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/nexus_response_api_teste/NexusResponseApiTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.nexus_response_api_teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusResponseApiTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
