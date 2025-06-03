4489b265930bb6479b292bcb73616595ede707d1
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-06-03 17:45:26 -0300

    pom

--- src/main/java/br/com/fiap/nexus_response_api_teste/NexusResponseApiTesteApplication.java
+++ src/main/java/br/com/fiap/nexus_response_api_teste/NexusResponseApiTesteApplication.java
@@ -2,8 +2,11 @@ package br.com.fiap.nexus_response_api_teste;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
+@OpenAPIDefinition(info = @Info(title = "NexusResponse API", version = "v1", description = "API do projeto Nexus Response", contact = @Contact(name = "Cauan Passos", email = "cauan.passos@nexusResponse.com")))
 public class NexusResponseApiTesteApplication {
 
 	public static void main(String[] args) {
